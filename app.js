f3efdaa98727b02907bbb23cb1e39b7e91cfc014
Prateek-Agarwal-20 <prateeka@sas.upenn.edu>
2017-07-17 14:32:57 -0700

    set up express

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+// var RtmClient = require('@slack/client').RtmClient;
+// var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+//
+// var bot_token = process.env.SLACK_BOT_TOKEN || '';
+//
+// var rtm = new RtmClient(bot_token);
+//
+// // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
+// rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+//   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+// });
+//
+// rtm.start();
+
+
 /*
     GOOGLE CALENDAR OAUTH2
     sample:
@@ -6,18 +21,21 @@
     https://github.com/google/google-api-nodejs-client#retrieve-authorization-code
 */
 
+var express = require('express');
+var app = express();
+
 var google = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
 
 var oauth2Client = new OAuth2(
-  '479081305544-e9r3ktb1108v2gisvd42sor7c9542abh.apps.googleusercontent.com',
-  'ESrUzekQrRhqMA5UjIl_OC5Z',
+  '479081305544-bql64pmv7ob5aktf7i1mocicf4vvcn4p.apps.googleusercontent.com',
+  'UFB_e08W8doSnrtXlEV1_0VI',
   'http://localhost:3000'
 );
 
 var url = oauth2Client.generateAuthUrl({
   // 'online' (default) or 'offline' (gets refresh_token)
-  access_type: 'online',
+  access_type: 'offline',
 
   // If you only need one scope you can pass it as a string
   scope: 'https://www.googleapis.com/auth/calendar',
@@ -28,10 +46,6 @@ var url = oauth2Client.generateAuthUrl({
 
 console.log(url);
 
-// oauth2Client.getToken(code, function (err, tokens) {
-//   // Now tokens contains an access_token and an optional refresh_token. Save them.
-//   if (!err) {
-//     //oauth2Client.setCredentials(tokens);
-//     console.log(code);
-//   }
-// });
+app.listen(3000, function(){
+  console.log('App listening on port 3000!');
+})
